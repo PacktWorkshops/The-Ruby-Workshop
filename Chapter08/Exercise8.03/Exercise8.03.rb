c691561c115bd405391c795a5486e1de2c7112eb
Snehal Tambe <45845632+packtsnehalt@users.noreply.github.com>
2019-10-24 12:32:14 +0530

    Update Exercise8.03.rb

--- Chapter08/Exercise8.03/Exercise8.03.rb
+++ Chapter08/Exercise8.03/Exercise8.03.rb
@@ -1,7 +1,5 @@
-require 'byebug'
-
-nums = [1, 2, 3, "four"]
-
-nums.each do |num|
-  byebug if num.class != Integer
-end
+require 'logger'
+log_bytes = 100
+log_limit = 5
+logger = Logger.new("log.txt", log_limit, log_bytes)
+logger.debug("Testing Log Rollover")
