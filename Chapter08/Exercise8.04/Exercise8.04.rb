e2bb20fbc06bad7d0a77037650410d1891689e1d
Snehal Tambe <45845632+packtsnehalt@users.noreply.github.com>
2019-10-24 12:33:33 +0530

    Update Exercise8.04.rb

--- Chapter08/Exercise8.04/Exercise8.04.rb
+++ Chapter08/Exercise8.04/Exercise8.04.rb
@@ -1,11 +1,5 @@
-counter = 0
-users = ["Matt", "Pete", "Brenton", "Geoff"]
-
-def greet_user(user)
-  puts "Hello #{user}"
-end
-
-users.each do |user|
-  greet_user user
-  counter += 1
+require 'byebug'
+nums = [1, 2, 3, "four"]
+nums.each do |num|
+  byebug if num.class != Integer
 end
